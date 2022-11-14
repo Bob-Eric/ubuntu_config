8b5a0e0f461252726b8712e1a7e9ff10f3736caf
Bob-Eric <1274653465@qq.com>
2022-11-14 17:38:13 +0800

    second version

--- install_software.sh
+++ install_software.sh
@@ -1,1 +1,22 @@
-/home/chy/Desktop/install_software.sh
+wget http://fishros.com/install -O fishros && . fishros
+sudo rm /etc/apt/sources.list.d/ros-fish.list
+gpg --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+gpg --export C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 | sudo tee /usr/share/keyrings/ros.gpg > /dev/null
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/ros.gpg] https://mirrors.ustc.edu.cn/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+wget https://github.com/v2fly/v2ray-core/releases/download/v4.31.0/v2ray-linux-64.zip
+wget https://github.com/Qv2ray/Qv2ray/releases/download/v2.7.0/Qv2ray-v2.7.0-linux-x64.AppImage
+sudo apt install vim git python3 g++ snap -y
+sudo snap install code --classic
+
+gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
+
+sudo add-apt-repository ppa:solaar-unifying/stable
+sudo apt update
+sudo apt install copyq solaar -y
+sudo apt install zsh curl gedit -y
+sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)" -y
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+chsh -s /bin/zsh 
+gedit ~/.zshrc
+gedit ~/.bashrc
